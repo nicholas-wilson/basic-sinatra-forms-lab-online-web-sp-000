39a68561d75114cff73906633e8d8a8e9e8b0938
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 06:20:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,13 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 end
